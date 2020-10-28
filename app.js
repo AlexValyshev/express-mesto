c413823cc68328d03d3153a69bb8a3526136a6ea
Alex Valyshev <avalyshev@mail.ru>
2020-10-29 00:22:15 +0300

    feat:Project-13

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
+/* eslint-disable import/order */
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
-// const bodyParser = require('body-parser');
 const router = require('./routes/index.js');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyParser.json(router));
-
 app.use(router);
-
 app.listen(PORT, () => {
   console.log(`App listening on port localhost:${PORT}`);
 });
--- controllers/post-users.js
+++ controllers/post-users.js
@@ -2,11 +2,10 @@
 // const readFile = require('../utils/read-file.js');
 const User = require('../models/user.js');
 
-// const jsonUsers = path.join(__dirname, '..', 'data', 'users.json');
-console.log(User);
+// const jsonUsers = path.join(__dirname, '..', 'data', 'users.json');;
 const postUsers = (req, res) => {
   // readFile(jsonUsers);
-  const { name, about, avatar } = req;
+  const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' })); // данные не записались, вернём ошибку
