a9c3e7a2f4f1d547a07db46bfd70b7076fb120b6
AlexValyshev <avalyshev@mail.ru>
2020-11-05 23:49:36 +0300

    Создать хелперы

--- models/card.js
+++ models/card.js
@@ -12,13 +12,8 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     validate: {
-<<<<<<< HEAD
-      validator(v) { // validator - функция проверки данных. v - значение свойства link
-        return regex.test(v); // если проверка Url по требованиям прошла, то вернется true
-=======
       validator(v) {
         return regex.test(v);
->>>>>>> ada5229a62c3624a5269bf1d9a3c540f2098c530
       },
       message: 'Введена неправильная ссылка',
     },
